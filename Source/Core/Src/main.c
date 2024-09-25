9e7b5e310302e73327cb821231a8d50bbba6c29e
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2024-09-25 17:17:39 +0700

    Implement Helper function enableColums() and setColums()

--- Source/Core/Src/main.c
+++ Source/Core/Src/main.c
@@ -57,6 +57,8 @@ void update7SEG(int index);
 void updateClockBuffer(void);
 void clearAllRows(void);
 void clearAllColums(void);
+void enableColums(int index, int state);
+void setColums(uint8_t number);
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
 /* USER CODE END PFP */
 
@@ -262,6 +264,47 @@ void clearAllColums(void){
 	HAL_GPIO_WritePin(ENM5_GPIO_Port, ENM5_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(ENM6_GPIO_Port, ENM6_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(ENM7_GPIO_Port, ENM7_Pin, GPIO_PIN_RESET);
+}
+
+void enableColums(int index, int state){
+	if(!state){
+		return;
+	}
+
+	switch(index){
+	case 0:
+		HAL_GPIO_WritePin(ENM0_GPIO_Port, ENM0_Pin, GPIO_PIN_SET);
+		break;
+	case 1:
+		HAL_GPIO_WritePin(ENM1_GPIO_Port, ENM1_Pin, GPIO_PIN_SET);
+		break;
+	case 2:
+		HAL_GPIO_WritePin(ENM2_GPIO_Port, ENM2_Pin, GPIO_PIN_SET);
+		break;
+	case 3:
+		HAL_GPIO_WritePin(ENM3_GPIO_Port, ENM3_Pin, GPIO_PIN_SET);
+		break;
+	case 4:
+		HAL_GPIO_WritePin(ENM4_GPIO_Port, ENM4_Pin, GPIO_PIN_SET);
+		break;
+	case 5:
+		HAL_GPIO_WritePin(ENM5_GPIO_Port, ENM5_Pin, GPIO_PIN_SET);
+		break;
+	case 6:
+		HAL_GPIO_WritePin(ENM6_GPIO_Port, ENM6_Pin, GPIO_PIN_SET);
+		break;
+	case 7:
+		HAL_GPIO_WritePin(ENM7_GPIO_Port, ENM7_Pin, GPIO_PIN_SET);
+		break;
+	default:
+		break;
+	}
+}
+
+void setColums(uint8_t number){
+	for(int i=0;i<MAX_LED_MATRIX;i++){
+		enableColums(i, (int)(number & (1 << i)));
+	}
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
