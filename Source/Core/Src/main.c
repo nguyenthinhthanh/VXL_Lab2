d361c30d94894e78bee362465bf28695b4ac162f
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2024-09-25 16:56:41 +0700

    Done Exercise8

--- Source/Core/Src/main.c
+++ Source/Core/Src/main.c
@@ -289,6 +289,7 @@ int main(void)
   {
 	/*I done Exercise 6 because SoftWare Timer had done before*/
 	/* I done Exercise 7 because I had done it in Exercise6*/
+	/* I done Exercise 8 because I had done it in Exercise6*/
 	if(Timer_Flag[0]){
 		update7SEG(index_led++);
 		if(index_led >= MAX_LED){
